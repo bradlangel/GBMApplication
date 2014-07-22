752aa51e88b7501ce7b377d9b57de3280fd8d647
bradlangel <b.a.langel@gmail.com>
2014-07-21 23:11:53 -0500

    added picasso functionality

--- app/src/main/java/com/bradlangel/gasbyme/GasStationAdaptor.java
+++ app/src/main/java/com/bradlangel/gasbyme/GasStationAdaptor.java
@@ -2,8 +2,6 @@ package com.bradlangel.gasbyme;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +9,10 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.List;
 
 /**
@@ -23,7 +25,8 @@ public class GasStationAdaptor extends BaseAdapter {
     String preference;
 
     //Max length of String(distance, price) to be displayed on ListView output
-    final int maxLength = 4;
+    final int maxLength = 2;
+
 
     GasStationAdaptor(Context context, List<GasStation> gasStationList, String preference) {
         this.context = context;
@@ -61,69 +64,88 @@ public class GasStationAdaptor extends BaseAdapter {
         LayoutInflater mInflater = (LayoutInflater) context
                 .getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
 
-        if (convertView == null) {
 
-            convertView = mInflater.inflate(R.layout.list_item, null);
-            holder = new ViewHolder();
+        convertView = mInflater.inflate(R.layout.list_item, null);
+        holder = new ViewHolder();
 
-            holder.gas_pic = (ImageView) convertView.findViewById(R.id.gas_pic);
-            holder.station_name = (TextView) convertView.findViewById(R.id.station_name);
-            holder.distance = (TextView) convertView.findViewById(R.id.distance);
-            holder.price = (TextView) convertView.findViewById(R.id.price);
-
-
-            GasStation gasStationPosition = gasStationList.get(position);
+        holder.gas_pic = (ImageView) convertView.findViewById(R.id.gas_pic);
+        holder.station_name = (TextView) convertView.findViewById(R.id.station_name);
+        holder.distance = (TextView) convertView.findViewById(R.id.distance);
+        holder.price = (TextView) convertView.findViewById(R.id.price);
 
 
-            //TODO: Set imageResource! Use picassa
-            holder.station_name.setText(gasStationPosition.getLongName());
-            holder.distance.setText(Double.toString(gasStationPosition.getDistance()).substring(0, maxLength) + " mi");
+        GasStation gasStationPosition = gasStationList.get(position);
 
-            //See which gas price to get
-            if(preference.equals("regular")) {
 
-                Double regularPrice =  gasStationPosition.getRegular();
-                //If we want regular gas price but station has none then set holder to null
-                if(regularPrice == null) {
-                    holder = null;
-                } else {
-                    holder.price.setText("$" + Double.toString(regularPrice).substring(0, maxLength));
-                }
-            } else if(preference.equals("premium")) {
+        //Use Picasso to place images into ImageView
+        Picasso.with(context)
+                .load(gasStationPosition.getImageUrl())
+                .resize(70, 70)
+                .into(holder.gas_pic);
 
-                Double premiumPrice =  gasStationPosition.getPremium();
-                //If we want premium gas price but station has none then set holder to null
-                if(premiumPrice == null) {
-                    holder = null;
-                } else {
-                    holder.price.setText("$" + Double.toString(premiumPrice).substring(0, maxLength));
-                }
-            } else if(preference.equals("plus")) {
 
-                Double plusPrice =  gasStationPosition.getPlus();
-                //If we want plus gas price but station has none then set holder to null
-                if(plusPrice == null) {
-                    holder = null;
-                } else {
-                    holder.price.setText("$" + Double.toString(plusPrice).substring(0, maxLength));
-                }
-            } else if(preference.equals("diesel")) {
 
-                Double dieselPrice =  gasStationPosition.getDiesel();
-                //If we want diesel gas price but station has none then set holder to null
-                if(dieselPrice == null) {
-                    holder = null;
-                } else {
-                    holder.price.setText("$" + Double.toString(dieselPrice));
-                }
+        //Place data into text view variables
+        holder.station_name.setText(gasStationPosition.getLongName());
+
+        holder.distance.setText(Double.toString(round(gasStationPosition.getDistance(), maxLength)) + " mi");
+
+        /*
+         * Use preferences to find correct price to place in text view variable
+         */
+        if(preference.equals("regular")) {
+
+            Double regularPrice =  gasStationPosition.getRegular();
+            //If we want regular gas price but station has none then set holder to null
+            if(regularPrice == 0.0) {
+                holder.price.setText("No Regular");
             } else {
-                holder.price.setText("BAD PREFERENCE!: " + preference);
+                holder.price.setText("$" + Double.toString(round(regularPrice, maxLength)));
             }
+        } else if(preference.equals("premium")) {
 
-            convertView.setTag(holder);
+            Double premiumPrice =  gasStationPosition.getPremium();
+            //If we want premium gas price but station has none then set holder to null
+            if(premiumPrice == 0.0) {
+                holder.price.setText("No Premium");
+            } else {
+                holder.price.setText("$" + Double.toString(round(premiumPrice, maxLength)));
+            }
+        } else if(preference.equals("plus")) {
+
+            Double plusPrice =  gasStationPosition.getPlus();
+            //If we want plus gas price but station has none then set holder to null
+            if(plusPrice == 0.0) {
+                holder.price.setText("No Plus");
+            } else {
+                holder.price.setText("$" + Double.toString(round(plusPrice, maxLength)));
+            }
+        } else if(preference.equals("diesel")) {
+
+            Double dieselPrice =  gasStationPosition.getDiesel();
+            //If we want diesel gas price but station has none then set holder to null
+            if(dieselPrice == 0.0) {
+                holder.price.setText("No Diesel");
+            } else {
+                holder.price.setText("$" + Double.toString(round(dieselPrice, maxLength)));
+            }
         } else {
-            holder = (ViewHolder)convertView.getTag();
+
+            holder.price.setText("BAD PREFERENCE!: " + preference);
         }
+
+
+
+        convertView.setTag(holder);
+
         return convertView;
+    }
+
+    public static double round(double value, int places) {
+        if (places < 0) throw new IllegalArgumentException();
+
+        BigDecimal bd = new BigDecimal(value);
+        bd = bd.setScale(places, RoundingMode.HALF_UP);
+        return bd.doubleValue();
     }
 }
